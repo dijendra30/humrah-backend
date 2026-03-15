698dceeba3767d4be9b07c75e8077a1f56a2c4f8
dijendra30 <95948269+dijendra30@users.noreply.github.com>
2026-03-15 09:29:51 +0530

    Update FoodCommentModel.js

--- models/FoodCommentModel.js
+++ models/FoodCommentModel.js
@@ -1,1 +1,39 @@
-FoodCommentModel.js
+const mongoose = require('mongoose');
+
+const FoodCommentSchema = new mongoose.Schema(
+  {
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'FoodPost',
+      required: true,
+      index: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      index: true,
+    },
+    text: {
+      type: String,
+      required: [true, 'Comment text is required'],
+      maxlength: [120, 'Comment cannot exceed 120 characters'],
+      trim: true,
+      validate: [
+        {
+          validator: (v) => !/(\+?\d[\s\-\.]?){7,}/g.test(v),
+          message: 'Comments cannot contain phone numbers',
+        },
+        {
+          validator: (v) => !/https?:\/\//i.test(v),
+          message: 'Comments cannot contain links',
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+FoodCommentSchema.index({ postId: 1, createdAt: -1 });
+
+module.exports = mongoose.model('FoodComment', FoodCommentSchema);
