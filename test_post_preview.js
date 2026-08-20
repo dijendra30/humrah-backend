6e017b48391eb835ad5ce9494cea400a2cd36e01
dijendra30 <95948269+dijendra30@users.noreply.github.com>
2026-08-20 10:13:51 +0530

    Delete test_post_preview.js

--- test_post_preview.js
+++
@@ -1,8 +0,0 @@
-const { generatePostPreview } = require('./utils/SharePreviewGenerator');
-const post = {
-    _id: '6a5f28b2a6e5459bcce85c4b',
-    userId: { firstName: 'Rahul', lastName: 'Saini' },
-    caption: 'I love Nature??',
-    imageUrl: 'https://res.cloudinary.com/dipycpjoy/image/upload/w_1200,h_630,c_fill/v1784621233/humrah/posts/d2lim5g0hqb7m12ubwtw.jpg'
-};
-console.log(generatePostPreview(post));
